f88bc5252e28b15acd8422d3459f578b52bd17c0
Carlos Neto <carlosalbertoneto28@gmail.com>
2022-07-07 15:15:06 -0300

    Order, Instant, ISO 8601 part 2

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.curso.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.curso.entities.Order;
 import com.example.curso.entities.Person;
+import com.example.curso.repositories.OrderRepository;
 import com.example.curso.repositories.PersonRepository;
 
 @Configuration
@@ -17,14 +20,21 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private PersonRepository personRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 	
 		Person p1 = new Person(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		Person p2 = new Person(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 
-		personRepository.saveAll(Arrays.asList(p1,p2));
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), p1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), p2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), p1);
 		
+		personRepository.saveAll(Arrays.asList(p1,p2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 	
 	
--- src/main/java/com/example/curso/entities/Order.java
+++ src/main/java/com/example/curso/entities/Order.java
@@ -12,49 +12,62 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private Person client;
-	
+
 	public Order() {
 	}
+
 	public Order(Long id, Instant moment, Person client) {
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Instant getMoment() {
 		return moment;
 	}
+
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
+
 	public Person getClient() {
 		return client;
 	}
+
 	public void setClient(Person client) {
 		this.client = client;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -66,5 +79,5 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
