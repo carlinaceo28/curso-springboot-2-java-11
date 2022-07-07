a0d179e5bdc3eb9af32a68cbf3e0f8fcd562fa62
Carlos Neto <carlosalbertoneto28@gmail.com>
2022-07-07 17:21:27 -0300

    Category

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.curso.entities.Category;
 import com.example.curso.entities.Order;
 import com.example.curso.entities.Person;
 import com.example.curso.entities.enums.OrderStatus;
+import com.example.curso.repositories.CategoryRepository;
 import com.example.curso.repositories.OrderRepository;
 import com.example.curso.repositories.PersonRepository;
 
@@ -23,20 +25,29 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-	
-		Person p1 = new Person(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		Person p2 = new Person(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, p1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, p2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, p1);
+		Person p1 = new Person(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		Person p2 = new Person(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, p1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, p2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, p1);
+
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
-		personRepository.saveAll(Arrays.asList(p1,p2));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		personRepository.saveAll(Arrays.asList(p1, p2));
+		
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
-	
-	
+
 }
