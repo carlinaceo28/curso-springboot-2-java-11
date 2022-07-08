ac683ba9bdf9461930947b2789d692b380ca3284
Carlos Neto <carlosalbertoneto28@gmail.com>
2022-07-08 14:08:05 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -36,13 +36,10 @@ public class TestConfig implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		Person p1 = new Person(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		Person p2 = new Person(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, p1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, p2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, p1);
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
 
 		Product pr1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product pr2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
@@ -50,13 +47,27 @@ public class TestConfig implements CommandLineRunner {
 		Product pr4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product pr5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		
-		Category cat1 = new Category(null, "Electronics");
-		Category cat2 = new Category(null, "Books");
-		Category cat3 = new Category(null, "Computers");
 		
 		productRepository.saveAll(Arrays.asList(pr1,pr2,pr3,pr4,pr5));
 		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
+		pr1.getCategories().add(cat2);
+		pr2.getCategories().add(cat1);
+		pr2.getCategories().add(cat3);
+		pr3.getCategories().add(cat3);
+		pr4.getCategories().add(cat3);
+		pr5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(pr1,pr2,pr3,pr4,pr5));
+		
+		
+		Person p1 = new Person(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		Person p2 = new Person(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, p1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, p2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, p1);
 		
 		personRepository.saveAll(Arrays.asList(p1, p2));
 		
